3f242309c949e1c5a790b4e260ba44a4675c6162
Nathan Anderson <nathan.anderson.1024@gmail.com>
2022-08-10 20:18:45 -0700

    finished day 2 daily challenge

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ This is a multi line comment
 */
 console.log('My Name is Nathan');
 console.log('My favorite food is Pizza!');
+
+// Day 2 Daily Challenge
+console.log('Supercalifragilisticexpialidocious'.length);
+
+// Log any number
+console.log(9);
+
+// Log a BigInt
+console.log(43245645434343n);
